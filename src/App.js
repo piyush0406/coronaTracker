67e9715a0f9b1a4d7aeabde926808cdb29a46f31
piyush0406 <pshroy@gmail.com>
2020-09-02 16:36:53 +0530

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import coronaImage from './images/QS.png';
 
-
 // import Cards from './components/Cards/Cards';
 // import Chart from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
@@ -9,6 +8,16 @@ import coronaImage from './images/QS.png';
 import { Cards, Chart, CountryPicker, Footer } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api' ;
+import ReactGA from 'react-ga';
+//import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "UA-177100276-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+//ReactGA.set({
+//  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+//})
 
 
 class App extends React.Component {

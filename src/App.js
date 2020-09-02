95c20e6f254339598a21e5a8c379846798fb4352
piyush0406 <pshroy@gmail.com>
2020-09-02 16:46:16 +0530

    test google analytics

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ import coronaImage from './images/QS.png';
 import { Cards, Chart, CountryPicker, Footer } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api' ;
-import ReactGA from 'react-ga';
-//import auth from './auth.ts'; // Sample authentication provider
-
-const trackingId = "UA-177100276-1"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-//ReactGA.set({
-//  userId: auth.currentUserId(),
-  // any data that is relevant to the user session
-  // that you would like to track with google analytics
-//})
-
 
 class App extends React.Component {
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 
 import App from './App'
 
+import ReactGA from 'react-ga';
+//import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "UA-177100276-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+//ReactGA.set({
+//  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+//})
+
 ReactDOM.render(<App /> , document.getElementById('root'));
